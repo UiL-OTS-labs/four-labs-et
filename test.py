db682059952134bf5c502a6dd16b7fbe4bf7be37
M.J.A.Duijndam@uu.nl <duijn119@hum.uu.nl>
2020-02-21 13:32:47 +0100

    Run test with new calibration

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import numpy
 import constants
 import pygame
 import os
+from pygaze import libinput
 from pygaze import libscreen
 from pygaze import libsound
 from pygaze import libtime
@@ -15,16 +16,50 @@ from pygaze import liblog
 from pygaze import libgazecon
 
 # # # # #
-# test the experimeny
+# test the experiment
+
+def infant_calibration(x, y):
+	calscreen = libscreen.Screen()
+	stim = numpy.random.randint(0, 3, 1)
+	if stim == 0:
+		im = 'stimuli/calibration/girl.png'
+	if stim == 1:
+		im = 'stimuli/calibration/fish.png'
+	if stim == 2:
+		im = 'stimuli/calibration/teddy.png'
+
+	size = .3
+	calscreen.draw_image(im, pos =(x, y), scale = size)
+	sound = libsound.Sound(soundfile = 'stimuli/calibration/chime-up.wav')
+	sound.play()
+	
+	disp.fill(calscreen)
+	disp.show()
+	for k in range(0, 23):
+		size -= .01
+		calscreen.clear()
+		calscreen.draw_image(im, pos =(x, y), scale = size)
+		disp.fill(calscreen)
+		disp.show()
+		#libtime.pause(40)
+	
+	sound = libsound.Sound(soundfile = 'stimuli/calibration/chime-down.wav')
+	sound.play()
+
+# # # # #
+# test the experiment
 
 # # # # #
 # experiment setup
+# keyboard
+kb = libinput.Keyboard(timeout=2000)
 
 # create display object
 disp = libscreen.Display()
 
 # create eyetracker object
 tracker = eyetracker.EyeTracker(disp)
+tracker.set_draw_calibration_target_func(infant_calibration)
 
 # Logfile
 log = liblog.Logfile()
@@ -37,7 +72,6 @@ wb = dispx * .266
 xl = dispx * .01
 xr = dispx - (dispx * .01 + wb)
 ya = (dispy - wb)/2
-
 							
 # create screens
 # attention grabber screen
@@ -78,34 +112,104 @@ bg = pygame.image.load('stimuli/background.png').convert()
 bgscr = libscreen.Screen()
 bgscr.draw_image(bg)
 
+calscr = libscreen.Screen()
+calscr.draw_text('press ESC to recalibrate')
+
 ## original stimuli screens
-orim = []
-reward = {}
+##orim = []
+##reward = {}
+##files = os.listdir('frames')
+##for i in range(6):
+##	reward[i] = {}
+##	orim.append(i) 
+##	orim[i] = []
+##	f = os.listdir('frames/'+ files[i])
+##	f.sort()
+##	ii = 0
+##	for frame in f:
+##		orim[i].append(pygame.image.load('frames/'+ files[i] + '/' + frame).convert())
+##		reward[i][ii] = libscreen.Screen()
+##		reward[i][ii].draw_image(orim[i][ii])
+##		ii += 1
+
 files = os.listdir('frames')
-for i in range(6):
-	reward[i] = {}
-	orim.append(i) 
-	orim[i] = []
-	f = os.listdir('frames/'+ files[i])
-	f.sort()
-	ii = 0
-	for frame in f:
-		orim[i].append(pygame.image.load('frames/'+ files[i] + '/' + frame).convert())
-		reward[i][ii] = libscreen.Screen()
-		reward[i][ii].draw_image(orim[i][ii])
-		ii += 1
 
-#reward = {}
-#for i in range(6):
-#	reward[i] = {}
-#	for k in range(24):
-#		reward[i][k] = libscreen.Screen()
-#		reward[i][k].draw_image(orim[i][k])
+f = os.listdir('frames/'+ files[0])
+f.sort()
+orim = []
+ii = 0
+reward0 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[0] + '/' + frame).convert())
+	reward0[ii] = libscreen.Screen()
+	reward0[ii].draw_image(orim[ii])
+	ii += 1
 
+f = os.listdir('frames/'+ files[1])
+f.sort()
+orim = []
+ii = 0
+reward1 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[1] + '/' + frame).convert())
+	reward1[ii] = libscreen.Screen()
+	reward1[ii].draw_image(orim[ii])
+	ii += 1	
 
+f = os.listdir('frames/'+ files[2])
+f.sort()
+orim = []
+ii = 0
+reward2 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[2] + '/' + frame).convert())
+	reward2[ii] = libscreen.Screen()
+	reward2[ii].draw_image(orim[ii])
+	ii += 1
 
+f = os.listdir('frames/'+ files[3])
+f.sort()
+orim = []
+ii = 0
+reward3 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[3] + '/' + frame).convert())
+	reward3[ii] = libscreen.Screen()
+	reward3[ii].draw_image(orim[ii])
+	ii += 1
+
+f = os.listdir('frames/'+ files[4])
+f.sort()
+orim = []
+ii = 0
+reward4 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[4] + '/' + frame).convert())
+	reward4[ii] = libscreen.Screen()
+	reward4[ii].draw_image(orim[ii])
+	ii += 1
+
+f = os.listdir('frames/'+ files[5])
+f.sort()
+orim = []
+ii = 0
+reward5 = {}
+for frame in f:
+	orim.append(pygame.image.load('frames/'+ files[5] + '/' + frame).convert())
+	reward5[ii] = libscreen.Screen()
+	reward5[ii].draw_image(orim[ii])
+	ii += 1
+
+reward = {}
+reward[0] = reward0
+reward[1] = reward1
+reward[2] = reward2
+reward[3] = reward3
+reward[4] = reward4
+reward[5] = reward5
+	
 # Define AOIs
-caoi = libgazecon.AOI('circle', (dispx / 2, dispy / 2), 250)
+caoi = libgazecon.AOI('circle', (dispx / 2, dispy / 2), 750)
 laoi = libgazecon.AOI('circle', (dispx / 6, dispy / 2), size = dispx / 3)
 raoi = libgazecon.AOI('circle', ((dispx / 6) * 5, dispy / 2), size = dispx / 3)
 
@@ -128,10 +232,17 @@ block_trial = 0
 
 # calibrate eye tracker
 tracker.calibrate()
-
+disp.fill(calscr)
+disp.show()
+key, time = kb.get_key()
+while key == 'escape':
+        tracker.calibrate()
+        disp.fill(calscr)
+        disp.show()
+        key, time = kb.get_key()
 
 # start the pre-switch trails
-for trial in range(18):
+for trial in range(36):
 	if trial == 9:
 		if left:
 			left = False
@@ -142,7 +253,23 @@ for trial in range(18):
 		else:
 			triplet_order = 0
 		block_trial = 0
-		
+
+	if trial >= 18:
+                block_trial = numpy.random.randint(0, 9, 1)
+                if numpy.random.randint(0, 2, 1) == 0:
+                        switch = True
+                else:
+                        switch = False
+                if switch:
+                        if left:
+                                left = False
+                        else:
+                                left = True
+                        if triplet_order == 0:
+                                triplet_order = 1
+                        else:
+                                triplet_order = 0
+
 	# start eye tracking
 	tracker.start_recording()
 	tracker.status_msg("now running trial %d" % trial)
@@ -160,7 +287,7 @@ for trial in range(18):
 
 	t00 = libtime.clock.get_time()
 	while (libtime.clock.get_time() - t00) < 400:
-		scale += .005
+		scale += .0025
 		bgscr.draw_image(surface, scale = scale)
 		disp.fill(bgscr)
 		disp.show()
@@ -195,7 +322,7 @@ for trial in range(18):
 			
 			t00 = libtime.clock.get_time()
 			while (libtime.clock.get_time() - t00) < 400:
-				scale += .005
+				scale += .0025
 				bgscr.draw_image(surface, scale = scale)
 				disp.fill(bgscr)
 				disp.show()
@@ -205,7 +332,20 @@ for trial in range(18):
 			count += 1
 		if count == 5:
 			tracker.stop_recording()
-			tracker.calibrate()
+			key, time = kb.get_key()
+			if key == 'escape':
+                                tracker.close()
+                                log.close()
+                                disp.close()
+			
+                        disp.fill(calscr)
+                        disp.show()
+                        key, time = kb.get_key()
+                        while key == 'escape':
+                                tracker.calibrate()
+                                disp.fill(calscr)
+                                disp.show()
+                                key, time = kb.get_key()
 			tracker.start_recording()
 			count = 0
 
